d99890fad0be3361ba1ee9b43f97653c1d4fee03
kk47 <kk47yx@gmail.com>
2018-11-08 20:28:03 +0800

    Update validator candidate according the configuration

--- src/common/general.h
+++ src/common/general.h
@@ -110,6 +110,8 @@ namespace bumo {
 
 		const static int64_t REWARD_PERIOD = (5 * 365 * 24 * 60 * 60) / 10;
 		const static int64_t REWARD_INIT_VALUE = 8 * 100000000;
+		
+		const static int32_t MAX_VALIDATORS = 50;
 
 		typedef enum WARNINGCODE_ {
 			WARNING,
