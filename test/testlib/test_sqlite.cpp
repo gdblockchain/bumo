e9d56b64fd585cfa5bf95012851747be47d3fdc1
xianguangtuan <463517114@qq.com>
2018-11-13 14:39:13 +0800

    Add some Comments
    add Comments

--- test/testlib/test_sqlite.cpp
+++ test/testlib/test_sqlite.cpp
@@ -8,12 +8,14 @@ int Callback(void *, int, char**, char**){
 
 int TestSqlite3(){
 	sqlite3* db;
+	//open database file
 	int ret = sqlite3_open("c:\\tmp\\sqlite3_test", &db);
 	if ( ret != 0 ){
 		LOG_ERROR_ERRNO("Failed to open db", STD_ERR_CODE, STD_ERR_DESC);
 	}
 
 	char *err = 0;
+	//define sql string
 	std::string sql = "select * from test";
 	ret = sqlite3_exec(db, sql.c_str(), Callback, 0, &err);
 	if (ret != SQLITE_OK)
