93814d734e74b4bc8a5af68ca13fb92730dd1851
shangsony <shangsong@buaa.edu.cn>
2018-11-14 09:55:37 +0800

    Update private_key_test.cpp
    Delete Chinese annotation

--- test/gtest/test/private_key_test.cpp
+++ test/gtest/test/private_key_test.cpp
@@ -17,7 +17,6 @@ protected:
 	bubi::PrivateKey* priv_key;
 };
 
-//测试PrivateKey类返回公私钥和地址不为空
 TEST_F(PrivateKeyTest, KeyNotNULL){
 
 	EXPECT_TRUE(priv_key->IsValid());
@@ -26,7 +25,6 @@ TEST_F(PrivateKeyTest, KeyNotNULL){
 	EXPECT_STRNE("", priv_key->GetBase58Address().c_str());
 };
 
-//测试PrivateKey类使用带参构建函数创建的对象返回的公司钥和地址不为空
 TEST_F(PrivateKeyTest,PrivateKey2){
 	bubi::PrivateKey priv_key2(priv_key->GetBase58PrivateKey());
 	EXPECT_TRUE(priv_key2.IsValid());
@@ -35,30 +33,28 @@ TEST_F(PrivateKeyTest,PrivateKey2){
 	EXPECT_STRNE("", priv_key2.GetBase58Address().c_str());
 };
 
-//测试签名
 TEST_F(PrivateKeyTest, Sign){
 	EXPECT_STRNE("", priv_key->Sign("").c_str());
 };
 
-//测试验证
 TEST_F(PrivateKeyTest, Verify){
 	std::string data="Hello";
 	std::string signature;
-	//生成公私钥匙
+
 	ed25519_secret_key raw_priv_key_;
 	ed25519_public_key raw_pub_key_;
 	ed25519_randombytes_unsafe(raw_priv_key_, sizeof(raw_priv_key_));
 	ed25519_publickey(raw_priv_key_, raw_pub_key_);
-	//签名
+
 	ed25519_signature sig;
 	ed25519_sign((unsigned char *)data.c_str(), data.size(), raw_priv_key_, raw_pub_key_, sig);
 	signature.append((const char *)sig, sizeof(sig));
 
-	//对公钥进行base58编码
+	
 	std::string key;
 	key.append((const char *)raw_pub_key_, sizeof(raw_pub_key_));
 	std::string publicKey = utils::Base58::Encode(key);
-	//执行测试验证
+	
 	EXPECT_TRUE(false);
 	EXPECT_TRUE(bubi::PublicKey::Verify(data, signature, publicKey));
 	EXPECT_FALSE(bubi::PublicKey::Verify(data + "1", signature, publicKey));
