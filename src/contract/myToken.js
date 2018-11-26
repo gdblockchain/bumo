c8f1ef2bc2ef7257ac0f640efc5c6eba7ae2518b
JianGuo <zjg555543@163.com>
2018-11-26 18:21:21 +0800

    Update myToken.js

--- src/contract/myToken.js
+++ src/contract/myToken.js
@@ -1,1 +1,10 @@
 'use strict';
+function init(input_str){
+    let params = JSON.parse(input_str).params;
+    assert(stoI64Check(params.supply) === true, 'Args check failed.');
+    let attribute = {};
+    attribute.supply = params.supply;
+    
+    storageStore('global', JSON.stringify(attribute));
+    storageStore(sender, attribute.supply);
+}
