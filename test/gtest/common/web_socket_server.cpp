0fc7bf7123be6b5b4758694f858de0bec526bea8
sonyshang <shangsong@buaa.edu.cn>
2018-11-27 13:38:58 +0800

    Update web_socket_server.cpp

--- test/gtest/common/web_socket_server.cpp
+++ test/gtest/common/web_socket_server.cpp
@@ -1967,8 +1967,7 @@ namespace bumo{
 
 		msg_processor_.SetCurrentRequestTest(SETCONFG);
 
-		for (int i = 0; i < (sizeof(testconditions) / 4); i++)
-		{
+		for (int i = 0; i < (sizeof(testconditions) / 4); i++){
 			msg_processor_.SetCurrentCondition(testconditions[i]);
 			RequestTest();
 		}
