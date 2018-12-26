4328f1a1525108c4983c0ee9caf68468d94d80c1
dingyouzhen <dingyouzhen@outlook.com>
2018-12-26 12:54:19 +0800

    add takerOrder

--- src/contract/dex.js
+++ src/contract/dex.js
@@ -1,6 +1,6 @@
 let globalAttribute = {};
 const globalAttributeKey = 'global_attribute';
-const 1BU = 100000000;
+const 1BU                = 100000000;
 
 function makeOrder(own, target, fee, expiration){
     assert(blockTimestamp < expiration, 'Order date has expired.');
@@ -19,7 +19,7 @@ function makeOrder(own, target, fee, expiration){
     else if(own.code === undefined){ /* CTP */
         assert(target.issuer === undefined, 'Must have a party asset is BU.');
 
-        let checkParam = { 'method':'allowance', 'params':{ 'owner':sender, 'spender':thisAddress } };
+        let checkParam = { 'method':'allowance', 'params':{ 'own':sender, 'spender':thisAddress } };
         let res = payCoin(own.issuer, '0', checkParam);
         assert(own.value === res.allowance, 'Insufficient CTP(contract address:' + own.issuer + ') paid.');
     }
@@ -33,13 +33,66 @@ function makeOrder(own, target, fee, expiration){
     }
     
     let orderKey   = 'order_' + (globalAttribute.orderInterval[1] + 1);
-    let orderValue = {'own':own, 'target':target, 'fee':fee, 'expiration':expiration};
+    let orderValue = {'maker': sender, 'own':own, 'target':target, 'fee':fee, 'expiration': expiration};
     storageStore(orderKey, JSON.stringify(orderValue));
 
     globalAttribute.orderInterval[1] = globalAttribute.orderInterval[1] + 1;
     storageStore(globalAttributeKey, JSON.stringify(globalAttribute));
 }
 
+function takeOrder(orderKey, fee){
+    let orderStr = storageLoad(orderKey);
+    assert(orderStr !== false, 'Order: ' + orderKey + ' does not exist');
+    let order = JSON.parse(orderStr);
+
+    let bilateralFee = 0;
+    if(order.target.issuer === undefined){ /* taker is BU */
+        if(int64Add(order.target.value, fee) === thisPayCoinAmount){/*平单*/
+            bilateralFee               = int64Add(fee, fee);
+            globalAttribute.serviceFee = int64Add(globalAttribute.serviceFee, bilateralFee);
+            payCoin(order.maker, int64Sub(thisPayCoinAmount, bilateralFee));
+
+            if(order.own.code === undefined){ /*maker is CTP*/
+                let transferFrom = { 'method':'transferFrom', 'params':{ 'from':order.maker, 'to':sender, 'value':order.own.value}};
+                payCoin(order.own.issuer, 0, transferFrom);
+                tlog(orderKey, order.maker, (order.own.issuer + ':' +  order.own.value), sender, order.target.value);
+            }
+            else{ /*maker is ATP*/
+                payAsset(sender, order.own.issuer, order.own.code, order.own.value);
+                tlog(orderKey, order.maker, (order.own.issuer + ':' + order.own.code + ':' + order.own.value), sender, order.target.value);
+            }
+        }
+    }
+    else if(order.target.code === undefined){ /*taker is CTP*/
+        let checkParam = { 'method':'allowance', 'params':{ 'own':sender, 'spender':thisAddress } };
+        let res = payCoin(order.target.issuer, '0', checkParam);
+        if(order.target.value === res.allowance){ /*平单*/
+            bilateralFee = int64Add(order.own.fee, order.own.fee);
+            globalAttribute.serviceFee = int64Add(globalAttribute.serviceFee, bilateralFee);
+
+            let transferFrom = { 'method':'transferFrom', 'params':{ 'from':sender, 'to':order.maker, 'value':order.target.value}};
+            payCoin(order.target.issuer, 0, transferFrom);
+            payCoin(sender, int64Sub(order.own.value, bilateralFee));
+
+            tlog(orderKey, order.maker, order.own.value, sender, (order.target.issuer + ':' + order.target.value));
+        }
+    }
+    else{ /*take is ATP*/
+        if((order.target.issuer === thisPayAsset.key.issuer) &&
+           (order.target.code === thisPayAsset.key.code) && 
+           (order.target.value === thisPayAsset.amount)){
+
+            bilateralFee = int64Add(order.own.fee, order.own.fee);
+            globalAttribute.serviceFee = int64Add(globalAttribute.serviceFee, bilateralFee);
+
+            payAsset(order.maker, thisPayAsset.key.issuer, thisPayAsset.key.code, thisPayAsset.amount);
+            payCoin(sender, int64Sub(order.own.value, bilateralFee));
+            tlog(orderKey, order.maker, order.own.value, sender, (order.target.issuer + ':' + order.target.code + ':' + order.target.value));
+        }
+    }
+
+    storageDel(orderKey);
+}
 
 function init(input_str){
     let params = JSON.parse(input_str).params;
@@ -47,6 +100,7 @@ function init(input_str){
     globalAttribute.owner = params.owner || sender;
     globalAttribute.feeRate = params.feeRate || 50000;
     globalAttribute.version = params.version || '1.0';
+    globalAttribute.serviceFee = 0;
     globalAttribute.orderInterval = [0, 0];
 
     storageStore(globalAttributeKey, JSON.stringify(globalAttribute));
@@ -63,7 +117,7 @@ function main(input_str){
         cancelOrder(input.params.order);
     }
     else if(input.method === 'takeOrder'){
-        takeOrder(input.params.order);
+        takeOrder(input.params.order, input.params.fee);
     }
     else if(input.method === 'updateFeeRate'){
         updateFeeRate(input.params.rate);
